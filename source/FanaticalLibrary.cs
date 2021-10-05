d1c3f18504c40e74635e2781d6c8415ee5eaf80a
nosyn00b <nosyn00b71@gmail.com>
2021-10-05 03:47:13 +0200

    Add project files.

---
+++ source/FanaticalLibrary.cs
@@ -0,0 +1,87 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FanaticalLibrary
+{
+    public class FanaticalLibrary : LibraryPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private FanaticalLibrarySettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("ef17cc27-95d4-45e7-bd49-214ba2e5f4b2");
+
+        // Change to something more appropriate
+        public override string Name => "Custom Library";
+
+        // Implementing Client adds ability to open it via special menu in playnite.
+        public override LibraryClient Client { get; } = new FanaticalLibraryClient();
+
+        public FanaticalLibrary(IPlayniteAPI api) : base(api)
+        {
+            settings = new FanaticalLibrarySettingsViewModel(this);
+            Properties = new LibraryPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        {
+            // Return list of user's games.
+            return new List<GameMetadata>()
+            {
+                new GameMetadata()
+                {
+                    Name = "Notepad",
+                    GameId = "notepad",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "notepad.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"c:\Windows\notepad.exe")
+                },
+                new GameMetadata()
+                {
+                    Name = "Calculator",
+                    GameId = "calc",
+                    GameActions = new List<GameAction>
+                    {
+                        new GameAction()
+                        {
+                            Type = GameActionType.File,
+                            Path = "calc.exe",
+                            IsPlayAction = true
+                        }
+                    },
+                    IsInstalled = true,
+                    Icon = new MetadataFile(@"https://playnite.link/applogo.png"),
+                    BackgroundImage = new MetadataFile(@"https://playnite.link/applogo.png")
+                }
+            };
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new FanaticalLibrarySettingsView();
+        }
+    }
+}
